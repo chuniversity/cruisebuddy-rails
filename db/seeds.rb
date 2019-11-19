ca3c19abd6d4e5dada17544dd018c65ce198ad81
Don Morehouse <dm.wyncode@gmail.com>
2019-11-19 21:19:14 +0000

    modified:   db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# p User.find_or_create_by!(email: 'example@mail.com' , password: '123123123' , password_confirmation: '123123123')
+
+p User.create!(email: 'example@mail.com' , password: '123123123' , password_confirmation: '123123123')
+
+cruise_line = CruiseLine.find_or_create_by!(name: Faker::Lorem.words(number: 4).join(' '))
+p cruise_line
+p Ship.find_or_create_by!(name: Faker::Lorem.words(number: 1), cruise_line: cruise_line)
+
