f4161eaee44aa6ebb2efb0ccdf17f04fa9f483ef
Don Morehouse <dm.wyncode@gmail.com>
2019-11-19 21:45:52 +0000

    modified:   db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 
 # p User.find_or_create_by!(email: 'example@mail.com' , password: '123123123' , password_confirmation: '123123123')
 
+def fake_rating
+    (1..5).to_a.sample
+end
 user = User.create!(email: 'example@mail.com' , password: '123123123' , password_confirmation: '123123123')
 p user
 user_profile = UserProfile.find_or_create_by!(ex_email: user.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Internet.username)
@@ -16,6 +19,10 @@ cruise_line = CruiseLine.find_or_create_by!(name: Faker::Lorem.words(number: 4).
 p cruise_line
 ship = Ship.find_or_create_by!(name: Faker::Lorem.word.capitalize, cruise_line: cruise_line)
 p ship
-review = Review.find_or_create_by!(user_profile: user_profile, ship: ship, body: Faker::Lorem.paragraph)
+
+review = Review.find_or_create_by!(user_profile: user_profile, ship: ship, body: Faker::Lorem.paragraph, rating: fake_rating )
 p review
+comment = Comment.create!(review: review, user_profile: user_profile, body:Faker::Lorem.sentence)
+p comment
+p Helpful.find_or_create_by!(review: review, user_profile: user_profile)
 puts "PASSED"
