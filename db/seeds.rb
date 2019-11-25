14a902ddc5e25c8d0a9bd6a34bcf84e8c0eeef7a
adoniramj <adoniramjvargas@gmail.com>
2019-11-25 15:28:27 -0500

    modification to seeds file and refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 # p User.find_or_create_by!(email: 'example@mail.com' , password: '123123123' , password_confirmation: '123123123')
 
+require('seeds_data.rb')
+
 def fake_rating
     (1..5).to_a.sample
 end
@@ -30,66 +32,21 @@ user_profile = UserProfile.find_or_create_by!(ex_email: user.email, first_name: 
 end
 
 #Cruise Line
-cruiselines = ['Carnival', 'Royal Caribbean', 'Disney Cruise']
+
 
 cruiselines.each do |cruiseline|
   CruiseLine.find_or_create_by!(name: cruiseline)
 end
 
 #Ships
-ships_collection = {
-        1 => {
-              1 => {ship_name: 'Carnival Horizon', ship_url:  'https://www.carnival.com/~/media/Images/Ships/HZ/carnival-horizon/heros/carnival-horizon-standard-ship.jpg'
-                   },
-              2 => {ship_name: 'Carnival Vista', ship_url:  'https://upload.wikimedia.org/wikipedia/commons/b/bb/TRIESTE_NAVI_%2826741027796%29_%28cropped%29.jpg'
-                   },
-              3 => {ship_name: 'Carnival Breeze', ship_url: 'https://www.cruisehive.com/wp-content/uploads/2015/08/carnivalbreeze1small.jpg'
-                   },
-              4 => {ship_name: 'Carnival Magic', ship_url:  'https://i.imgur.com/lZJ4PxT.jpg'
-                   },
-              5 => {ship_name: 'Carnival Dream', ship_url:  'https://images.cruisecritic.com/image/5531/carnival-dream_600x300_21.jpg'
-                   }               
-              },
-        2 => {
-              6 => {ship_name: 'Freedom of the Seas', ship_url:  'https://live.staticflickr.com/2423/3934243662_7a5b0c50a8_b.jpg'
-                   },
-              7 => {ship_name: 'Liberty of the Seas', ship_url:  'https://d1xfoqsc632pe4.cloudfront.net/images/dyn/cc/429027094ab7ea4133e1f5.25409721-1254518315.jpg'
-                   },
-              8 => {ship_name: 'Symphony of the Seas', ship_url:  'https://clubthrifty.com/wp-content/uploads/2019/08/Symphony-of-the-Seas_Featured-Image.jpg'
-                   },
-              9 => {ship_name: 'Harmony of the Seas', ship_url:  'https://images.cruisecritic.com/image/5599/royal-caribbean-harmony-of-the-seas_600x300_21.jpg'
-                   },
-              10 => {ship_name: 'Oasis of the Seas', ship_url:  'https://d23n7ahjfnjotp.cloudfront.net/imgs/mobileheaderipad/ship_292_rci_oa_aerials_labadee-2-011f_1024x416_mobhdr.jpg'
-                   },
-              11 => {ship_name: 'Allure of the Seas', ship_url:  'https://images.cruisecritic.com/image/5556/royal-caribbean-allure-of-the-seas_600x300_21.jpg'
-                   }                                       
-              },
-        3 => {
-              12 => {ship_name: 'Disney Magic', ship_url:  'https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2018/08/wegfds47385-624x351.jpg'
-                   },
-              13 => {ship_name: 'Disney Wonder', ship_url:  'https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2016/11/ndwe4796785711.jpg'
-                   },
-              14 => {ship_name: 'Disney Dream', ship_url:  'https://www.picturethemagic.com/wp-content/uploads/2018/07/disney-dream-ship-castaway-cay-flickr.jpg'
-                   },
-              15 => {ship_name: 'Disney Fantasy', ship_url:  'http://www.disneyholidays.co.uk/disney-cruise-line/images/ships/mobile/disney-dream.jpg'
-                   }                    
-             }
-}
-
 ships_collection.each do |cruise_line_id, ships_ids|
-  # cruise_line_id
   ships_ids.each do |ship_id, ship_data|
-    # puts ship_id
-    # puts ship_data[:ship_name]
-    # puts ship_data[:ship_url]
-
     Ship.find_or_create_by!(
       name: ship_data[:ship_name], 
       cruise_line_id: cruise_line_id, 
       description: Faker::Lorem.paragraph(sentence_count: 10),
       url: ship_data[:ship_url]
     )
-
   end
 end
 
@@ -161,6 +118,8 @@ end
     price:price
   )
 end
+
+
 
 #Reviews
 (1..100).each do |i|
