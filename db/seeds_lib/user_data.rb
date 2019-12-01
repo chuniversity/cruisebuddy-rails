5e546f4d7818d6beeec9f71a1384814b7ea528a4
adoniramj <adoniramjvargas@gmail.com>
2019-12-01 11:14:03 -0500

    changed url to user_data.rb

--- db/seeds_lib/user_data.rb
+++ db/seeds_lib/user_data.rb
@@ -4,69 +4,69 @@ USER_DATA = [
     password_confirmation: '123123123',
     email:                 'sample@mail.com',
     first_name             'Natalia',
-    picture_url            'http://chunis.org/images/ships/users/female1.jpg'
+    picture_url            'http://chunis.org/images/users/female1.jpg'
   },
   {
     password:              '123123123',
     password_confirmation: '123123123',
     email:                 'sample1@mail.com',
     first_name             'Andrew',
-    picture_url            'http://chunis.org/images/ships/users/male1.jpg'
+    picture_url            'http://chunis.org/images/users/male1.jpg'
   },
   {
     password:              '123123123',
     password_confirmation: '123123123',
     email:                 'sample2@mail.com',
     first_name             'Sarah',
-    picture_url            'http://chunis.org/images/ships/users/female2.jpg'
+    picture_url            'http://chunis.org/images/users/female2.jpg'
   },
   {
     password:              '123123123',
     password_confirmation: '123123123',
     email:                 'sample3@mail.com',
     first_name             'Daniel',
-    picture_url            'http://chunis.org/images/ships/users/male2.jpg'
+    picture_url            'http://chunis.org/images/users/male2.jpg'
   },
   {
     password:              '123123123',
     password_confirmation: '123123123',
     email:                 'sample4@mail.com',
     first_name             'Jane',
-    picture_url            'http://chunis.org/images/ships/users/female3.jpg'
+    picture_url            'http://chunis.org/images/users/female3.jpg'
   },
   {
     password:              '123123123',
     password_confirmation: '123123123',
     email:                 'sample5@mail.com',
     first_name             'Joshua',
-    picture_url            'http://chunis.org/images/ships/users/male4.jpg'
+    picture_url            'http://chunis.org/images/users/male4.jpg'
   },
   {
     password:              '123123123',
     password_confirmation: '123123123',
     email:                 'sample6@mail.com',
     first_name             'Michelle',
-    picture_url            'http://chunis.org/images/ships/users/female4.jpg'
+    picture_url            'http://chunis.org/images/users/female4.jpg'
   },
   {
     password:              '123123123',
     password_confirmation: '123123123',
     email:                 'sample7@mail.com',
     first_name             'Jennifer',
-    picture_url            'http://chunis.org/images/ships/users/female5.jpg'
+    picture_url            'http://chunis.org/images/users/female5.jpg'
   },
   {
     password:              '123123123',
     password_confirmation: '123123123',
     email:                 'sample8@mail.com',
     first_name             'Andrea',
-    picture_url            'http://chunis.org/images/ships/users/female6.jpg'
+    picture_url            'http://chunis.org/images/users/female6.jpg'
   },
   {
     password:              '123123123',
     password_confirmation: '123123123',
     email:                 'sample9@mail.com',
     first_name             'Susan',
-    picture_url            'http://chunis.org/images/ships/users/female7.jpg'
+    picture_url            'http://chunis.org/images/users/female7.jpg'
   }
 ]
