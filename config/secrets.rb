dccd9b4ccf90aac010f85d80fbd4d161eebe9599
Chuniversity <contact@chunis.org>
2019-12-02 19:53:19 -0500

    changed key

--- config/secrets.rb
+++ config/secrets.rb
@@ -7,4 +7,4 @@ test:
 # Do not keep production secrets in the repository,
 # instead read values from the environment.
 production:
-  secret_key_base: my_secret_key_that_should_prolly_be_hidden
+  secret_key_base: my_secret_key
